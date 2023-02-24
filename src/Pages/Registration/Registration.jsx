6ab57a9383c4e1cb596b7ba4d8051b1d81cfa425
Anubhav <chakrabortyanubhav0@gmail.com>
2023-02-24 21:01:57 +0530

    ADD: leftpart of registrationform

--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -21,7 +21,17 @@ const Registration = ()=> {
             </div>
         </div>
         <div className={style.rightcontainer}>
-
+            <div className={style.coverpicontainer}>
+                <div className={style.coverimgcontainer}>
+                    <img src="/images/iplimg.png" />
+                </div>
+                <div className={style.descriptioncontainer}>
+                    <div className={style.descheading}>IPL AUCTION</div>
+                    <div className={style.description}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque numquam officiis delectus nam tenetur iusto mollitia magni voluptatum nisi? Tenetur.
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     );
