05af8b9bb7dd96325445ffe44f935159320727e0
Anubhav <chakrabortyanubhav0@gmail.com>
2023-02-25 22:38:55 +0530

    FIX: unique key props error

--- src/Components/Registrationform1/Form.jsx
+++ src/Components/Registrationform1/Form.jsx
@@ -1,30 +1,64 @@
 import style from "./Form.module.scss";
 
-const Form1 =({fields, handleMainFormChange}) => {
-    return (
-        <div className={style.formelementcontainer}>
-            <div className={style.inputcontainer}>
-                <input type="text" className={style.input} value={fields.teamName} name="teamName" onChange={(event) => handleMainFormChange(event)}  />
-            </div>
-            <div className={style.inputcontainer}>
-                <input type="text" className={style.input} value={fields.name} name="name" onChange={(event) => handleMainFormChange(event)}  />
-            </div>
-            <div className={style.inputcontainer}>
-                <input type="text" className={style.input} value={fields.phone} name="phone" onChange={(event) => handleMainFormChange(event)}  />
-            </div>
-            <div className={style.inputcontainer}>
-                <input type="text" className={style.input} value={fields.email} name="email" onChange={(event) => handleMainFormChange(event)}  />
-            </div>
-            <div className={style.inputcontainer}>
-                <select className={style.select} name="gender" onChange={(event) => handleMainFormChange(event)}>
-                    <option defaultValue={fields.gender} className={style.option}>Gender</option>
-                    <option className={style.option} value="Male">Male</option>
-                    <option className={style.option} value="Female">Female</option>
-                </select>
-            </div>
-            <div className={style.formendline}></div>
-        </div>
-    )
-}
+const Form1 = ({ fields, handleMainFormChange }) => {
+  return (
+    <div className={style.formelementcontainer}>
+      <div className={style.inputcontainer}>
+        <input
+          type="text"
+          className={style.input}
+          value={fields.teamName}
+          name="teamName"
+          onChange={(event) => handleMainFormChange(event)}
+        />
+      </div>
+      <div className={style.inputcontainer}>
+        <input
+          type="text"
+          className={style.input}
+          value={fields.name}
+          name="name"
+          onChange={(event) => handleMainFormChange(event)}
+        />
+      </div>
+      <div className={style.inputcontainer}>
+        <input
+          type="text"
+          className={style.input}
+          value={fields.phone}
+          name="phone"
+          onChange={(event) => handleMainFormChange(event)}
+        />
+      </div>
+      <div className={style.inputcontainer}>
+        <input
+          type="text"
+          className={style.input}
+          value={fields.email}
+          name="email"
+          onChange={(event) => handleMainFormChange(event)}
+        />
+      </div>
+      <div className={style.inputcontainer}>
+        <select
+          className={style.select}
+          name="gender"
+          onChange={(event) => handleMainFormChange(event)}
+        >
+          <option defaultValue={fields.gender} className={style.option}>
+            Gender
+          </option>
+          <option className={style.option} value="Male">
+            Male
+          </option>
+          <option className={style.option} value="Female">
+            Female
+          </option>
+        </select>
+      </div>
+      <div className={style.formendline}></div>
+    </div>
+  );
+};
 
 export default Form1;
--- src/Components/Registrationform2/Form2.jsx
+++ src/Components/Registrationform2/Form2.jsx
@@ -1,27 +1,55 @@
 import style from "./Form2.module.scss";
 
-const Form2 =({fields, handleFormChange, index}) => {
-    return (
-        <div className={style.formelementcontainer}>
-            <div className={style.inputcontainer}>
-                <input type="text" className={style.input} value={fields.name} name="name" onChange={(event) => handleFormChange(index,event)}  />
-            </div>
-            <div className={style.inputcontainer}>
-                <input type="text" className={style.input} value={fields.phone} name="phone" onChange={(event) => handleFormChange(index,event)}  />
-            </div>
-            <div className={style.inputcontainer}>
-                <input type="text" className={style.input} value={fields.email} name="email" onChange={(event) => handleFormChange(index,event)}  />
-            </div>
-            <div className={style.inputcontainer}>
-                <select className={style.select} name="gender" onChange={(event) => handleFormChange(index,event)}>
-                    <option defaultValue={fields.gender} className={style.option}>Gender</option>
-                    <option className={style.option} value="Male">Male</option>
-                    <option className={style.option} value="Female">Female</option>
-                </select>
-            </div>
-            <div className={style.formendline}></div>
-        </div>
-    )
-}
+const Form2 = ({ fields, handleFormChange, index }) => {
+  return (
+    <div className={style.formelementcontainer}>
+      <div className={style.inputcontainer}>
+        <input
+          type="text"
+          className={style.input}
+          value={fields.name}
+          name="name"
+          onChange={(event) => handleFormChange(index, event)}
+        />
+      </div>
+      <div className={style.inputcontainer}>
+        <input
+          type="text"
+          className={style.input}
+          value={fields.phone}
+          name="phone"
+          onChange={(event) => handleFormChange(index, event)}
+        />
+      </div>
+      <div className={style.inputcontainer}>
+        <input
+          type="text"
+          className={style.input}
+          value={fields.email}
+          name="email"
+          onChange={(event) => handleFormChange(index, event)}
+        />
+      </div>
+      <div className={style.inputcontainer}>
+        <select
+          className={style.select}
+          name="gender"
+          onChange={(event) => handleFormChange(index, event)}
+        >
+          <option defaultValue={fields.gender} className={style.option}>
+            Gender
+          </option>
+          <option className={style.option} value="Male">
+            Male
+          </option>
+          <option className={style.option} value="Female">
+            Female
+          </option>
+        </select>
+      </div>
+      <div className={style.formendline}></div>
+    </div>
+  );
+};
 
 export default Form2;
