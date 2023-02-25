d71b29948e5b98ae9e7ebd021d2762cdcfe681c0
hereisadi <hereisadi@proton.me>
2023-02-25 20:44:18 +0530

    ADD:speakers

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import { Button } from "../../Components";
+import Speakers from "../../Components/Homepage/Speakers/Speakers";
 
 import style from "./Home.module.scss";
 
@@ -7,6 +8,7 @@ const Home = () => {
     <main className={style.home}>
       <h1>Home Page for Srijan 2023</h1>
       <Button type="button" />
+      <Speakers />
     </main>
   );
 };
