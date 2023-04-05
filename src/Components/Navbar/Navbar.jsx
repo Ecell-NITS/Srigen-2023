978d07862cc7887cbfb4b44cafa1e83556941407
vivek-918 <vivektitan3@gmail.com>
2023-04-05 21:15:39 +0530

    FIX: eslint issues

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,70 @@
+import { useState } from "react";
+import { Pivot as Hamburger } from "hamburger-react";
 import style from "./Navbar.module.scss";
+
 const Navbar = () => {
-  return <nav className={style.navbar}>Navbar</nav>;
+  const [burger, setBurger] = useState(false);
+  const toggleMenu = () => {
+    setBurger(!burger);
+  };
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      toggleMenu();
+    }
+  };
+  return (
+    <nav className={style.navbar}>
+      <img className={style.logo} src="/logos/srijan-logo.svg" alt="srijan-logo"></img>
+      <div
+        className={style.hamburger}
+        role="button"
+        tabIndex={0}
+        onClick={toggleMenu}
+        onKeyDown={handleKeyDown}
+      >
+        <Hamburger color="teal" />
+      </div>
+      <div className={burger ? `${style.activate}` : `${style.menu}`}>
+        <ul className={style.mainMenu}>
+          <li className={`${style.active} ${style.item}`}>
+            <a className={style.links} href="#!">
+              Home
+            </a>
+          </li>
+          <li className={style.item}>
+            <a className={style.links} href="#!">
+              Gallery
+            </a>
+          </li>
+          <li className={style.item}>
+            <a className={style.links} href="#!">
+              Events
+            </a>
+          </li>
+          <li className={style.item}>
+            <a className={style.links} href="#!">
+              Workshops
+            </a>
+          </li>
+          <li className={style.item}>
+            <a className={style.links} href="#!">
+              Speakers
+            </a>
+          </li>
+          <li className={style.item}>
+            <a className={style.links} href="#!">
+              Team
+            </a>
+          </li>
+          <li className={style.item}>
+            <a className={style.links} href="#!">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
